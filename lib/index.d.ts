3087a03478d48b3afdf395f280b2c754fcb283cc
Jek <jekcom@gmail.com>
2024-06-20 10:20:36 +0300

    Added colors Split context, subContext in remote logger

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,37 +1,41 @@
-export declare type RemoteLoggerType = (args: any, level: number, context: (string | null)) => void;
+export declare type RemoteLoggerType = (args: any, level: string, context: string | null, subContext: string | null) => void;
 export interface LoggerOptions {
     remoteLogger: RemoteLoggerType;
 }
 export declare function configureLogger(options: LoggerOptions): void;
+declare enum ForeColor {
+    Black = "\u001B[30m",
+    Red = "\u001B[31m",
+    Green = "\u001B[32m",
+    Yellow = "\u001B[33m",
+    Blue = "\u001B[34m",
+    Magenta = "\u001B[35m",
+    Cyan = "\u001B[36m",
+    White = "\u001B[37m"
+}
+declare enum BgColor {
+    Black = "\u001B[40m",
+    Red = "\u001B[41m",
+    Green = "\u001B[42m",
+    Yellow = "\u001B[43m",
+    Blue = "\u001B[44m",
+    Magenta = "\u001B[45m",
+    Cyan = "\u001B[46m",
+    White = "\u001B[47m"
+}
 export default class Logger {
+    private bgColor;
+    private foreColor;
     static RemoteLogger: RemoteLoggerType | null;
     _context: string;
-    Reset: string;
-    Bright: string;
-    Dim: string;
-    Underscore: string;
-    Blink: string;
-    Reverse: string;
-    Hidden: string;
-    FgBlack: string;
-    FgRed: string;
-    FgGreen: string;
-    FgYellow: string;
-    FgBlue: string;
-    FgMagenta: string;
-    FgCyan: string;
-    FgWhite: string;
-    BgBlack: string;
-    BgRed: string;
-    BgGreen: string;
-    BgYellow: string;
-    BgBlue: string;
-    BgMagenta: string;
-    BgCyan: string;
-    BgWhite: string;
-    constructor(context: string);
-    logs(args: any, subContext?: (string | null)): void;
-    log(args: any, subContext?: (string | null), stringify?: boolean): void;
-    error(args: any, subContext?: (string | null)): void;
+    private Reset;
+    constructor(context: string, bgColor?: BgColor, foreColor?: ForeColor);
+    /**
+     * @deprecated The method should not be used. Use log() instead
+     */
+    logs(args: any, subContext?: string | null): void;
+    log(args: any, subContext?: string | null): void;
+    error(args: any, subContext?: string | null): void;
     private logRemote;
 }
+export {};
--- src/index.ts
+++ src/index.ts
@@ -1,100 +1,130 @@
-
-import dayjs = require('dayjs');
-export type RemoteLoggerType = (args: any, level: number, context: (string | null)) => void;
+import dayjs = require("dayjs");
+export type RemoteLoggerType = (
+  args: any,
+  level: string,
+  context: string | null,
+  subContext: string | null
+) => void;
 export interface LoggerOptions {
-    remoteLogger: RemoteLoggerType
+  remoteLogger: RemoteLoggerType;
 }
 export function configureLogger(options: LoggerOptions) {
-    Logger.RemoteLogger = options.remoteLogger;
+  Logger.RemoteLogger = options.remoteLogger;
+}
+
+enum ForeColor {
+  Black = "\x1b[30m",
+  Red = "\x1b[31m",
+  Green = "\x1b[32m",
+  Yellow = "\x1b[33m",
+  Blue = "\x1b[34m",
+  Magenta = "\x1b[35m",
+  Cyan = "\x1b[36m",
+  White = "\x1b[37m",
+}
+
+enum BgColor {
+  Black = "\x1b[40m",
+  Red = "\x1b[41m",
+  Green = "\x1b[42m",
+  Yellow = "\x1b[43m",
+  Blue = "\x1b[44m",
+  Magenta = "\x1b[45m",
+  Cyan = "\x1b[46m",
+  White = "\x1b[47m",
 }
 export default class Logger {
-    
-    static RemoteLogger: RemoteLoggerType | null = null;
+  static RemoteLogger: RemoteLoggerType | null = null;
 
-    _context: string;
+  _context: string;
 
+  private Reset = "\x1b[0m";
+//   Bright = "\x1b[1m";
+//   Dim = "\x1b[2m";
+//   Underscore = "\x1b[4m";
+//   Blink = "\x1b[5m";
+//   Reverse = "\x1b[7m";
+//   Hidden = "\x1b[8m";
 
+//   FgBlack = "\x1b[30m";
+//   FgRed = "\x1b[31m";
+//   FgGreen = "\x1b[32m";
+//   FgYellow = "\x1b[33m";
+//   FgBlue = "\x1b[34m";
+//   FgMagenta = "\x1b[35m";
+//   FgCyan = "\x1b[36m";
+//   FgWhite = "\x1b[37m";
 
-    Reset = '\x1b[0m';
-    Bright = '\x1b[1m';
-    Dim = '\x1b[2m';
-    Underscore = '\x1b[4m';
-    Blink = '\x1b[5m';
-    Reverse = '\x1b[7m';
-    Hidden = '\x1b[8m';
+//   BgBlack = "\x1b[40m";
+//   BgRed = "\x1b[41m";
+//   BgGreen = "\x1b[42m";
+//   BgYellow = "\x1b[43m";
+//   BgBlue = "\x1b[44m";
+//   BgMagenta = "\x1b[45m";
+//   BgCyan = "\x1b[46m";
+//   BgWhite = "\x1b[47m";
 
-    FgBlack = '\x1b[30m';
-    FgRed = '\x1b[31m';
-    FgGreen = '\x1b[32m';
-    FgYellow = '\x1b[33m';
-    FgBlue = '\x1b[34m';
-    FgMagenta = '\x1b[35m';
-    FgCyan = '\x1b[36m';
-    FgWhite = '\x1b[37m';
+  constructor(context: string, private bgColor : BgColor = BgColor.Yellow, private foreColor : ForeColor = ForeColor.Black ) {
+    this._context = context;
+  }
 
-    BgBlack = '\x1b[40m';
-    BgRed = '\x1b[41m';
-    BgGreen = '\x1b[42m';
-    BgYellow = '\x1b[43m';
-    BgBlue = '\x1b[44m';
-    BgMagenta = '\x1b[45m';
-    BgCyan = '\x1b[46m';
-    BgWhite = '\x1b[47m';
+  /**
+   * @deprecated The method should not be used. Use log() instead
+   */
+  logs(args: any, subContext: string | null = null) {
+    this.log(args, subContext);
+  }
 
+  log(args: any, subContext: string | null = null) {
+    let date = dayjs().format("HH:mm:ss.SSS ");
 
-    constructor(context: string) {
-        this._context = context;
+    // Stringify if passed object is not string
+    if (!(typeof args === "string" || args instanceof String)) {
+      args = JSON.stringify(args, null, 2);
     }
 
-    logs(args: any, subContext: (string | null) = null) {
-        this.log(args, subContext, true);
-    }
-    log(args: any, subContext: (string | null) = null, stringify = false) {
-
-        let date = dayjs().format('HH:mm:ss.SSS ');
-        if (Array.isArray(args) || stringify) {
-            args = JSON.stringify(args, null, 2);
-        }
-        let context = `[${this._context}]`;
-        if (subContext) {
-            context = (`[${this._context}]:[${subContext}]`);
-
-        }
-
-        console.log(`${this.BgYellow}${this.FgBlack}[${date}]${context}${this.Reset}: `, args);
-
-        this.logRemote(args, 2, context);
+    let context = `[${this._context}]`;
+    if (subContext) {
+      context = `[${this._context}]:[${subContext}]`;
     }
 
-    error(args: any, subContext: (string | null) = null) {
+    console.log(
+      `${this.bgColor}${this.foreColor}[${date}]${context}${this.Reset}: `,
+      args
+    );
 
-        let date = dayjs().format('HH:mm:ss.SSS ');
+    this.logRemote(args, "INFO", this._context, subContext);
+  }
 
-        if (args instanceof Error) {
-            args = args.message;
-        }
+  error(args: any, subContext: string | null = null) {
+    let date = dayjs().format("HH:mm:ss.SSS ");
 
-        args = JSON.stringify(args, null, 2);
-
-
-        let context = `[${this._context}]`;
-        if (subContext) {
-            context = (`[${this._context}]:[${subContext}]`);
-
-        }
-        console.log(`${this.BgRed}${this.FgBlack}[${date}]${context}${this.Reset}: `, args);
-
-        this.logRemote(args, 2, context);
+    if (args instanceof Error) {
+      args = args.message;
     }
 
+    args = JSON.stringify(args, null, 2);
 
-
-    private logRemote(args: any, level: number, context: (string | null)) {
-        if (Logger.RemoteLogger) {
-            Logger.RemoteLogger(args, level, context)
-        }
-
-
+    let context = `[${this._context}]`;
+    if (subContext) {
+      context = `[${this._context}]:[${subContext}]`;
     }
+    console.log(
+      `${BgColor.Red}${ForeColor.White}[${date}]${context}${this.Reset}: `,
+      args
+    );
 
+    this.logRemote(args, "ERROR", this._context, subContext);
+  }
+
+  private logRemote(
+    args: any,
+    level: string,
+    context: string | null,
+    subContext: string | null
+  ) {
+    if (Logger.RemoteLogger) {
+      Logger.RemoteLogger(args, level, context, subContext);
+    }
+  }
 }
